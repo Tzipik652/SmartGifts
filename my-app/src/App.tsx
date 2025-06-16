5837cd6c8891757345e5537d764d0c539686cc8f
Tzipik652 <tzipik652@gmail.com>
2025-06-16 23:55:12 +0300

    General update of files, added new files, deleted old files, and made changes to dependencies and styling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import './App.css'; 
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -10,13 +10,15 @@ import MyCart from './pages/MyCart';
 import Profile from './pages/Profile';
 import AddProduct from './pages/AddProduct';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import ProductsList from './pages/ProductsList';
 
 const App = () => {
   return (
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>}>
+          <Route path="/products" element={<PrivateRoute><ProductsList /></PrivateRoute>} />
           <Route path="/product/:id" element={<PrivateRoute><ProductDetails /></PrivateRoute>} />
           <Route path="/cart" element={<PrivateRoute><MyCart /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
